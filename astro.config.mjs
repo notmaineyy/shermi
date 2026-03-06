c2729de832e7cd2fa30b2f78d8152643c1cfb172
Shermaine <shermaineang2802@gmail.com>
2026-03-06 23:55:29 +0800

    fixed errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://shermi.dev', // ✏️ update with your domain
+  site: 'https://shermaineang.com', // ✏️ update with your domain
 });

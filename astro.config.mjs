e45e09e5192593e6c90f1ae2a3dd8c491bf5aa83
Shermaine <shermaineang2802@gmail.com>
2026-03-07 00:56:24 +0800

    fixed dependencies stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwindcss()],
   site: 'https://shermaineang.com', // ✏️ update with your domain
 });

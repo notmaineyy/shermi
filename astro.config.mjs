5123a0983de48547748f7565e7c954ef17540488
Shermaine <shermaineang2802@gmail.com>
2026-03-07 00:04:14 +0800

    updated dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://shermaineang.com', // ✏️ update with your domain
+
+  // ✏️ update with your domain
+  site: 'https://shermaineang.com',
+
+  output: 'server',
+  adapter: cloudflare()
 });

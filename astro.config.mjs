6f231ec3cfa8ac97bfb099bf3d67ddc8820fca7e
Shermaine <shermaineang2802@gmail.com>
2026-03-07 00:30:19 +0800

    fixed...?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-import cloudflare from '@astrojs/cloudflare';
-
 export default defineConfig({
   integrations: [tailwind()],
-
-  // ✏️ update with your domain
-  site: 'https://shermaineang.com',
-
-  output: 'server',
-  adapter: cloudflare()
+  site: 'https://shermaineang.com', // ✏️ update with your domain
 });
